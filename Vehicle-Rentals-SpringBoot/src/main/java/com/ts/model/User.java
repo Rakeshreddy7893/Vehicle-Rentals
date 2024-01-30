faa8fd532c2c17704bd17b531829943e722b6118
Ranjith Redddy <ranjithreddykondra@gmail.com>
2024-01-30 14:28:59 +0530

    User entity class created

--- Vehicle-Rentals-SpringBoot/src/main/java/com/ts/model/User.java
+++ Vehicle-Rentals-SpringBoot/src/main/java/com/ts/model/User.java
@@ -1,5 +1,18 @@
 package com.ts.model;
 
-public class User {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
+import lombok.Data;
+
+@Data
+@Entity
+public class User {
+	@Id
+	@GeneratedValue
+	private int userId;
+	private String userName;
+	private String email;
+	private String password;
 }
