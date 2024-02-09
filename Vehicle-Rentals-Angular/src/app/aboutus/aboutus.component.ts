355972253590f311de23ccc9ac705099b2f7fdee
Ranjith Redddy <ranjithreddykondra@gmail.com>
2024-02-10 00:48:59 +0530

    Added captcha

--- Vehicle-Rentals-Angular/src/app/aboutus/aboutus.component.ts
+++ Vehicle-Rentals-Angular/src/app/aboutus/aboutus.component.ts
@@ -11,7 +11,7 @@ export class AboutusComponent implements OnInit{
   constructor(private router:Router){}
   ngOnInit() {
     
-  }
+}
 
   registerMe(){
   this.router.navigate(['register']);
--- Vehicle-Rentals-Angular/src/app/login/login.component.ts
+++ Vehicle-Rentals-Angular/src/app/login/login.component.ts
@@ -53,7 +53,6 @@ export class LoginComponent implements OnInit {
     }
   }
   
-  
   togglePasswordVisibility() {
     this.showPassword = !this.showPassword;
   }
